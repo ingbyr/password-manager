87f081840d091b3dfe0a3a520141b27e9dc02666
ingbyr <ingbyr@outlook.com>
2020-06-09 17:07:46 +0800

    add comment

--- Backup.py
+++ Backup.py
@@ -5,13 +5,18 @@ from datetime import datetime
 from Common import backup_dir
 
 
+# 备份
 def backup(accounts):
     file = backup_dir.joinpath(datetime.now().strftime('%Y%m%d%H%M%S') + '.bin')
     pickle.dump(accounts, open(file, "wb"))
     return file
 
+
+# 显示所有备份
 def list_backups():
     return os.listdir(backup_dir)
 
+
+# 加载备份
 def load_backup(file):
     return pickle.load(open(file, "rb"))
